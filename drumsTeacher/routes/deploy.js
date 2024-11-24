1ce3f14b10e6394d7f8763ecc8ecc79fda7e5e03
Emerson Raniere de Macedo <56753061+emersonmacedo123@users.noreply.github.com>
2024-11-24 19:48:24 +0100

    finished with the deploy too

--- drumsTeacher/routes/deploy.js
+++ drumsTeacher/routes/deploy.js
@@ -7,47 +7,55 @@ const morgan = require("morgan");
 
 const Joi = require("joi");
 
-let activityAnalytics = [{
-    "qualAnalytics": [
-        { "name": "Analytic 1", "included": false },
-        { "name": "Analytic 2", "included": false }
-    ],
-    "quantAnalytics": [
-        { "name": "Analytic 3", "included": false },
-        { "name": "Analytic 4", "included": false }
-    ]
-}];
+// Middlewares 
+app.use(bodyParser.json()); // Para analisar o corpo das requisições JSON
 
-// Esquema de validação Joi
-const analyticSchema = Joi.object({
-    name: Joi.string().required(),
-    included: Joi.boolean().required()
+// JOI Validation
+const deploySchema = Joi.object({
+    activityID: Joi.string().required(),
+    InvenRAstdID: Joi.string().required(),
+    json_params: Joi.object({
+        "Analytic 1": Joi.string().optional(),
+        "Analytic 2": Joi.string().optional(),
+        "Analytic 3": Joi.boolean().optional(),
+        "Analytic 4": Joi.number().optional(),
+    }).required(),
 });
 
-const schema = Joi.object({
-    qualAnalytics: Joi.array().items(analyticSchema).required(),
-    quantAnalytics: Joi.array().items(analyticSchema).required()
-});
-
-
-//funcao GET em /activityAnalytics
-const activityAnalyticsGetter = (req, res) => {
-    res.send(activityAnalytics)
+// GET /deploy
+//functions
+const mensagemDeployGet = (req, res) => {
+    res.send("Voce acessou GET do deploy. Use POST para iniciar uma nova instancia de atividade");
 };
 
-router.get("/", activityAnalyticsGetter);
-//funcao POST em /configURL
+router.get("/", mensagemDeployGet);
+
 router.post("/", (req, res) => {
-    // Validação
-    const { error, value } = schema.validate(req.body);
+    //etapa de validacao
+    const { error, value } = deploySchema.validate(req.body);
     if (error) {
-        console.log('Erro na validação:', error.details);
-        res.status(400).send(`Erro na validação:`)
+        res.status(400).json({ error: `Validation Error: ${error.details[0].message}` });
     } else {
         console.log('Analytics configurado!', value);
-        activityAnalytics = value;
-        res.status(201).json(value);
+        // Dados validados
+        const { activityID } = req.body;
+        // Resposta com o URL de instância
+        const instanceURL = `http://localhost:5000/atividade/${activityID}`;
+        res.json({ activity_url: instanceURL });
     }
 })
+
+//exemplo de dados input
+// {
+//     "activityID": "12345",
+//     "InvenRAstdID": "98765",
+//     "json_params": {
+//         "Analytic 1": "Some text",
+//         "Analytic 2": "http://example.com",
+//         "Analytic 3": true,
+//         "Analytic 4": 42
+//     }
+// }
+
 
 module.exports = router;
